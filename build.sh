76e1a31ad28dfd5d89c520d144d956d4f7a56a31
SanderLentink.nl <sander@lent.ink>
2020-04-22 16:07:25 +0000

    removed links, which may avoided the publish guidelines of the chrome store

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 cp ./chrome-extension/* ./public/
 cd public
 mv pwdgen.htm index.html
-sed -i 's/<!--insert-h1-here-->/<h1>Password generator<\/h1>/g' index.html
+sed -i 's/<!--include-in-webversion//g' index.html
+sed -i 's/include-in-webversion-->//g' index.html
 cd ..
 zip -r pass-gen-chrome-ext.zip chrome-extension
